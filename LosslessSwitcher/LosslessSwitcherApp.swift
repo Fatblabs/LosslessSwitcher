9084139ff3299b4225b203f0086646eb6ff0ca11
Fatblabs <stuffducky@gmail.com>
2026-05-04 12:11:33 -0500

    Initial Commit

---
+++ LosslessSwitcher/LosslessSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  LosslessSwitcherApp.swift
+//  LosslessSwitcher
+//
+//  Created by Sean Nguyen on 5/4/26.
+//
+
+import SwiftUI
+
+@main
+struct LosslessSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
